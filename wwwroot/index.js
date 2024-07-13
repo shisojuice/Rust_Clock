ec29e97c724a94d32df3df9879bd5dfb8ea24438
shisojuice <>
2024-07-13 09:33:23 +0900

    最大1秒の遅延問題の修正

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -2,13 +2,15 @@ import init, { clock_png } from './rust_clock.js';
 
 async function run() {
     await init();
-    setInterval(()=>{
+    const mainImg = document.getElementById('mainImg');
+    (function loop() {
         const now = new Date();
         const h = now.getHours();
         const m = now.getMinutes();
         const s = now.getSeconds();
-        document.getElementById("mainImg").src = clock_png(h,m,s);
-    }, 1000);
+        mainImg.src = clock_png(h, m, s);
+        setTimeout(loop, 1000 - Date.now() % 1000);
+    })();
 }
 run();
 
